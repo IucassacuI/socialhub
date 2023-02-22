f9609a6b0eac91b7a7ddd3d6d940c1e6cd2f5877
IucassacuI <115167629+IucassacuI@users.noreply.github.com>
2023-02-22 13:35:19 -0300

    Update sh.rb

--- sh.rb
+++ sh.rb
@@ -6,88 +6,71 @@ require 'json'
 $users = PStore.new 'users'
 $cache = PStore.new 'cache'
 
-def exist user
-  !$users.transaction(true){$users[user]}.nil?
+def exist(user)
+  !$users.transaction(true) { $users[user] }.nil?
 end
 
-def login user, pass
-  
-  unless exist(user)
-    return ['/err?code=1', false]
-  end
+def login(user, pass)
+  return ['/err?code=1', false] unless exist(user)
 
-  dbpass = BCrypt::Password.new $users.transaction(true) {$users[user][:pass]}
-  
-  if dbpass == pass
-    return  ["/user/#{user}", true]
-  else
-    return ['/err?code=2', false]
-  end
+  dbpass = BCrypt::Password.new $users.transaction(true) { $users[user][:pass] }
+
+  return ["/user/#{user}", true] if dbpass == pass
+
+  ['/err?code=2', false]
 end
 
-def signup user, pass
+def signup(user, pass)
   supass = BCrypt::Password.create(pass).to_s
-  
-  $users.transaction{$users[user] = {pass: supass, insta: [], twit: [], yt: []}}
-  $cache.transaction{$cache[user] = {insta: [], twit: [], yt: []}}
+
+  $users.transaction { $users[user] = { pass: supass, insta: [], twit: [], yt: [], red: [] } }
+  $cache.transaction { $cache[user] = { insta: [], twit: [], yt: [], red: [] } }
 end
 
-def iusers user
-  $users.transaction(true){$users[user][:insta]}
+def follows(user, net)
+  $users.transaction(true) { $users[user][net] }
 end
 
-def tusers user
-  $users.transaction(true){$users[user][:twit]}
-end
+def update(user)
+  nets = {
+    'instagram' => [method(:iusers), :insta],
+    'twitter' => [method(:tusers), :twit],
+    'youtube' => [method(:yusers), :yt],
+    'reddit' => [method(:rsubs), :red]
+  }
 
-def yusers user
-  $users.transaction(true){$users[user][:yt]}
-end
+  nets.each_pair do |net, methodsym|
+    method = methodsym[0]
 
-def update user
+    method.call(user).each do |u|
+      posts = URI.open(URI("http://localhost:4567/#{net}/#{u}?truncate=1")).read
 
-  tusers(user).each do |u|
-    posts = URI.open(URI("http://localhost:4567/twitter/#{u}?truncate=1")).read
+      $cache.transaction do
+        sym = methodsym[1]
 
-    $cache.transaction do
-      tcache = $cache[user][:twit]
-      json = JSON.parse(posts)
-      
-      $cache[user][:twit].push json unless tcache.include? json
-    end
-  end
+        cache = $cache[user][sym]
+        json = JSON.parse(posts)
 
-  iusers(user).each do |u|
-    posts = URI.open(URI("http://localhost:4567/instagram/#{u}?truncate=1")).read
-
-    $cache.transaction do
-      icache =  $cache[user][:insta]
-      json = JSON.parse(posts)
-      
-      $cache[user][:insta].push json unless icache.include? json
-    end
-  end
-
-  yusers(user).each do |u|
-    posts = URI.open(URI("http://localhost:4567/youtube/#{u}?truncate=1")).read
-
-    $cache.transaction do
-      ycache = $cache[user][:yt]
-      json = JSON.parse(posts)
-      
-      $cache[user][:yt].push json unless ycache.include? json
+        $cache[user][sym].pop if $cache[user][sym].size > 5
+        $cache[user][sym] = [json] + $cache[user][sym] unless cache.include? json
+      end
     end
   end
 end
 
-def change platform, user, users
-  $users.transaction{$users[user][platform.to_sym] = users.split}
+def change(user, instagram, twitter, reddit, yt)
+  $users.transaction do
+    $users[user][:insta] = instagram.split
+    $users[user][:twit] = twitter.split
+    $users[user][:red] = reddit.split
+    $users[user][:yt] = yt.split
+  end
 end
 
-def cache user
+def cache(user)
   f = {}
 
-  usercache = $cache.transaction{$cache[user]}
+  usercache = $cache.transaction { $cache[user] }
 
   usercache.each_pair do |net, posts|
     f[net.to_s] = posts
@@ -96,7 +79,7 @@ def cache user
   f
 end
 
-def account user, newpass
+def account(user, newpass)
   bpass = BCrypt::Password.create(newpass).to_s
-  $users.transaction {$users[user][:pass] = bpass}
+  $users.transaction { $users[user][:pass] = bpass }
 end
